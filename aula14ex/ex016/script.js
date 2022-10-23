2687ee525b6ef1da4cb0d92c208e6d4425308f19
Eduardo Magno <du.magno@yahoo.com.br>
2022-10-23 18:06:40 -0300

    att

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,13 @@
+var botao = document.querySelector('#contar')
+botao.addEventListener('click', contar)
+
+function contar() {
+    var res = document.querySelector('#resultado')
+    res.innerHTML = `<p>Contando:</p>`
+    var inicio = document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passo = document.querySelector('input#passo')
+    for (Number.inicio; Number.inicio<= Number.fim; Number.inicio += Number.passo) {
+        res.innerHTML += `${Number.inicio}`
+    }
+}
