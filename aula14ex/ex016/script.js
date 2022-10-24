0d9635fdbdeea8ca7758c5e81655d693d255a14f
Eduardo Magno <du.magno@yahoo.com.br>
2022-10-24 17:25:21 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,10 +1,10 @@
 function contar() {
     var res = document.querySelector('div#resultado')
     res.innerHTML = `<p>Contando:</p>`
-    var inicio = document.querySelector('input#inicio')
-    var fim = document.querySelector('input#fim')
-    var passo = document.querySelector('input#passo')
-    for (Number.inicio; Number.inicio<= Number.fim; Number.inicio += Number.passo) {
+    let ini = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    for (Number.ini; Number.ini<= Number.fim; Number.ini += Number.passo) {
         res.innerHTML += `${Number.inicio}`
     }
 }
