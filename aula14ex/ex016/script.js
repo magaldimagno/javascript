812362ad873ae482b4fbcfebc448a7618a94bdbc
Eduardo Magno <du.magno@yahoo.com.br>
2022-10-26 20:24:30 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,36 +1,32 @@
 function contar() {
     let res = document.querySelector('div#resultado')
-    let ini = document.querySelector('input#txti')
-    let fim = document.querySelector('input#txtf')
-    let passo = document.querySelector('input#txtp')
+    let txt = [document.querySelector('input#txti'), document.querySelector('input#txtf'), document.querySelector('input#txtp')]
     
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (txt[0].value.length == 0 || txt[1].value.length == 0 || txt[2].value.length == 0) {
         // Dados incompletos
         res.innerHTML = '<p>Impossível contar, faltam dados!</p>'
         // window.alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = '<p>Contando: </p>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
+        let num = [Number(txt[0].value), Number(txt[1].value), Number(txt[2].value)]
 
-        if (p <= 0) {
+        if (num[2] <= 0) {
             // Passo 0 vira 1
             window.alert('Passo Inválido! Considerando PASSO 1')
-            p = 1
+            num[2] = 1
         }
 
         let cont = document.createElement('p')
         res.appendChild(cont)
 
-        if (f > i) {
+        if (num[1] > num[0]) {
             // Contagem crescente
-            for (let c = i; c <= f; c += p) {
+            for (let c = num[0]; c <= num[1]; c += num[2]) {
                 cont.innerHTML += `${c} \u{1F449} `
             }
         } else {
             // Contagem decrescente
-            for (let c = i; c >= f; c -= p) {
+            for (let c = num[0]; c >= num[1]; c -= num[2]) {
                 cont.innerHTML += `${c} \u{1F449} `
             }
         }
