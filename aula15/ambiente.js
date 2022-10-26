4720f48e26ff301b9a356331639490ef93ecff0f
Eduardo Magno <du.magno@yahoo.com.br>
2022-10-25 21:33:50 -0300

    .

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.push(7)
+console.log(num)
+console.log(`O 3º elemento do vetor é: ${num[2]}`)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O vetor organizado em ordem crescente é: ${num.sort()}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,5 @@
+
+
+for (let pos = 0; pos < num.length; pos++){
+    console.log(num[pos])
+}
